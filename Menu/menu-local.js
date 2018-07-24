574e182773f24b4929bc171c1163eae77aa1da10
GabrielSegura <seguragabriel@unbosque.edu.co>
2018-07-24 15:59:25 -0500

    diplomado files

---
+++ Menu/menu-local.js
@@ -0,0 +1,2 @@
+//Banner top
+$('#banner-home').html('<img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/amgen/images/assets/diplomado-banner-principal.png" alt="Diplomado Expertos en Lípidos"/>');
