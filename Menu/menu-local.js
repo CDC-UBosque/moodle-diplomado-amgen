829f717ec5e2e3cd0b76caf277ea8e5c711eafd9
GabrielSegura <seguragabriel@unbosque.edu.co>
2018-07-27 08:41:59 -0500

    ajustes nuevas fuentes

--- Menu/menu-local.js
+++ Menu/menu-local.js
@@ -1,2 +1,5 @@
 //Banner top
 $('#banner-home').html('<img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/amgen/images/assets/diplomado-banner-principal.png" alt="Diplomado Expertos en Lípidos"/><h1>Diplomado<br><span>Expertos en Lípidos</span></h1>');
+
+// footer curso amgen
+$('#footer').html('<div class="span6"><img src="images/logo-amgen.svg" alt="AMGEN" /></div><div class="span6"><img src="images/logo-UEB.svg" alt="Universidad El Bosque" /></div><div class="span12"><p>© Todos los derechos reservados</p></div>');
